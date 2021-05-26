711250eb174fa274b5c9d4625ac7238d84ad6348
Payal Valera <pajeb4you@outlook.com>
2021-05-26 19:34:06 +0530

    Code clean-up

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import { CONFIG } from './config';
 import { HttpServer } from './httpServer';
 
 new HttpServer()
-    .getServer(false)
+    .getServer()
     .listen(
         CONFIG.portNumber,
         () => {
